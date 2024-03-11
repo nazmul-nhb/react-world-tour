d3238718d413ae68529229e6653dec180418f47c
nazmul-nhb <nazmulnhb007@yahoo.com>
2024-03-11 12:52:16 +0600

    added dynamic button

--- src/components/Country/Country.jsx
+++ src/components/Country/Country.jsx
@@ -1,10 +1,23 @@
-import './Country.css';
-const Country = ({country}) => {
-const {name, flags} = country;
+import { useState } from 'react';
+import './Country.css'
+
+const Country = ({ country }) => {
+    const { name, flags, area, population, cca3 } = country;
+    const [visited, setVisited] = useState(false);
+
+    const handleVisited = () => {
+        setVisited(!visited);
+    }
+
     return (
         <div className="country">
-            <h3>Name: {name?.common}</h3>
-            <img src={flags.png} alt={name?.common} />
+            <h3>{name.common}</h3>
+            <img className='flag' src={flags.png} alt={name.common} />
+            <p>Population: {population} </p>
+            <p>Area: {area} </p>
+            <p>Code: {cca3} </p>
+            <button onClick={handleVisited}>{visited ? 'Visited' : 'Going'}</button>
+            <p>{visited ? 'I have visited this country!' : 'I want to visit this country.'}</p>
         </div>
     );
 };
